c7a3a14fcace6b89e256b113aee149f59f04936b
Prabhuprasadkarmi <139318142+Prabhuprasadkarmi@users.noreply.github.com>
2026-01-18 18:42:59 +0530

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,30 @@
+#1 This is the first Program of the Python 
+print("hello world!")
+
+#2 Python input and output 
+
+name = input("Enter your name: ")
+print("Hello,", name, "! Welcome!")
+
+# output 
+
+# Enter your name: Prabhu
+# Hello, Prabhu ! Welcome! 
+
+#3 Python printing variable
+
+#3.1 integer 
+a = 10
+print(a)
+
+#3.2 string 
+name = "Prabhu"
+print(name)
+
+#3.3 double
+b = 2.4
+print(b)
+
+
+# multiple variable output 
+print(a,name,b)
