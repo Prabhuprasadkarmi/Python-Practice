2948ff0ed0cf0af0942dfb88a4f61ee452d28c14
Prabhu <prabhuprasadkarmi@gmail.com>
2026-01-18 21:28:09 +0530

    know about the multiple input

--- Day1.py
+++ Day1.py
@@ -28,3 +28,18 @@ print(b)
 
 # multiple variable output 
 print(a,name,b)
+
+
+# Take multiple input 
+
+# *** splitting the values entered by
+#  the user into separate variables for each value using the split() method
+
+x, y = input("Enter number of boys and girls: ").split()   #  where split() used the space as separation
+print("Number of boys: ", x)
+print("Number of girls: ", y)
+ 
+x, y, z = input("Enter total no. of student and boys and girls: ").split()
+print("Total number of students: ", x)
+print("Number of boys is : ", y)
+print("Number of girls is : ", z)
