8bcdfa3685210514137c8d9fa6215732f61e0674
Prabhu <prabhuprasadkarmi@gmail.com>
2026-01-19 22:00:01 +0530

    Excercise.py is modified

--- Excercise.py
+++ Excercise.py
@@ -1,11 +1,11 @@
-# swapping the two number without using the third variable
+#1. swapping the two number without using the third variable
 
 a,b = 5, 10
 a,b = b,a
 print(a,b)
 
-# print the length of the string using len()
+#2. print the length of the string using len()
 
-name = "Prabhuprasad Karmi"  # here the space is count as the length
+name = "Prabhuprasad Karmi"                   # here the space is count as the length
 length = len(name)
 print("the length of the name :",length)
