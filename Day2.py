6aaaeaf9c83e5a698fddb26c3877c5040d7e2ae2
Prabhu <prabhuprasadkarmi@gmail.com>
2026-01-19 19:22:17 +0530

    Learn about the Python variable and rules

---
+++ Day2.py
@@ -0,0 +1,30 @@
+# What is Python variable
+
+# variable is the container of the data which can be manipulated at the time of program execution.
+# Assigning of the variable in python using the = operator
+
+x = 5
+name = "Prabhu"  
+print(x)
+print(name)
+
+# Rules of Naming of variable.
+# variable must contain the letters,digitsand(_).
+# variable does not start with digit.
+# variable name is case sensitive .
+
+
+# Python dynamically store the multiple datatype value.
+
+a = 10 
+a = "Rajababu"
+
+# Assign the same value to multiple variable
+
+a = b = c = 10
+print(a)
+print(b)
+print(c)
+
+print(a,b,c)
+
