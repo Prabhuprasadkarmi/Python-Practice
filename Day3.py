827991b14d5dccd842ffe2db3c08628b2bb63618
Prabhu <prabhuprasadkarmi@gmail.com>
2026-01-20 23:36:26 +0530

    learning about the arithematic operator and comparision operator

--- Day3.py
+++ Day3.py
@@ -1,2 +1,51 @@
 # What is Python Operator
 
+# operator are used in the operation on the variables
+
+# operator = special symbol like + ,- , *, /,% etc.
+# operand = on which the operator are used 
+
+# Types of operators
+
+# 1. Arithematic operator
+   # Basic mathematical operation like addition,subtraction,multiplication,division
+
+# Variables
+a = 15
+b = 3
+
+# Addition
+print("Addition:", a + b)  
+
+# Subtraction
+print("Subtraction:", a - b) 
+
+# Multiplication
+print("Multiplication:", a * b)  
+
+# Division
+print("Division:", a / b) 
+
+# Floor Division
+print("Floor Division:", a // b)  # its give the only integer value of the division
+
+# Modulus
+print("Modulus:", a % b) # its give the remainder of the division
+
+# Exponentiation
+print("Exponentiation:", a ** b) # it multiple a into b times
+
+print(a*a*a)
+
+# comparision operator
+    # comparing the variable and returns the true and false
+
+a = 13
+b = 33
+
+print(a > b)    # is a greater than b
+print(a < b)    # is a lesser than b
+print(a == b)   # is a equal to b where = sign give the error
+print(a != b)   # is a not equal to b
+print(a >= b)   # is a greater than equal to b
+print(a <= b)   # is a less than equal to b
