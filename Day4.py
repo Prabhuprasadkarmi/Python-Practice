7a87a514a82615c60bb15f3e89a90eb79ee06bca
Prabhu <prabhuprasadkarmi@gmail.com>
2026-01-21 19:38:44 +0530

    starting of practice Day 4

---
+++ Day4.py
@@ -0,0 +1,20 @@
+#  Bitwise operator
+   
+     # Bitwise oprator act on the bits and operate bit-by-bit operation
+
+# Types of the Bitwise operator
+    # bitwise NOT
+    # bitwise AND
+    # bitwise OR
+    # bitwise SHIFT
+    # bitwise XOR
+
+a = 10
+b = 4
+
+print(a & b)
+print(a | b)
+print(~a)
+print(a ^ b)
+print(a >> 2)
+print(a << 2)
