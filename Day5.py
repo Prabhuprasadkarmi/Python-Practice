c67954506d86d838238933370a0f0239eda10715
Prabhu <prabhuprasadkarmi@gmail.com>
2026-01-22 22:58:19 +0530

    learn to create list datatype

--- Day5.py
+++ Day5.py
@@ -12,7 +12,7 @@
 # Strings in Python can be created using single quotes, double quotes or even triple quotes.
 # We can access individual characters of a String using index.
 
-s = 'Welcome to the Geeks World'
+s = 'Welcome to the Real World'
 print(s)
 
 # check data type 
@@ -28,3 +28,19 @@ print(s[-1])
 # Lists are similar to arrays found in other languages. 
 # They are an ordered and mutable collection of items. 
 # It is very flexible as items in a list do not need to be of the same type.
+
+# Creating a List in Python
+
+# Lists in Python can be created by just placing sequence inside the square brackets[].
+
+
+
+
+# Empty list
+a = []
+# list with int values
+a = [1, 2, 3]
+print(a)
+# list with mixed values int and String
+b = ["Raja", "Babu", "karmi", 4, 5]
+print(b)
