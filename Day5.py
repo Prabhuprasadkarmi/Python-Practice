4f8485d4697a81131e63a576d2941f7010e6e41e
Prabhu <prabhuprasadkarmi@gmail.com>
2026-01-22 22:51:12 +0530

    learn about the string datatypes

---
+++ Day5.py
@@ -0,0 +1,24 @@
+# Sequence datatypes
+
+# A sequence is an ordered collection of items, which can be of similar or different data types. 
+# Sequences allow storing of multiple values in an organized and efficient fashion.
+# There are several sequence data types of Python:
+
+# String datatype
+
+# Python Strings are arrays of bytes representing Unicode characters.
+#  In Python, there is no character data type, a character is a string of length one. It is represented by str class.
+
+# Strings in Python can be created using single quotes, double quotes or even triple quotes.
+# We can access individual characters of a String using index.
+
+s = 'Welcome to the Geeks World'
+print(s)
+
+# check data type 
+print(type(s))
+
+# access string with index
+print(s[1])
+print(s[2])
+print(s[-1])
