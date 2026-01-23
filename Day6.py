2fe8361d7ce7ce02f06b5e9bf6644ccacd9359ad
Prabhu <prabhuprasadkarmi@gmail.com>
2026-01-23 20:10:20 +0530

    Acessing the list items

---
+++ Day6.py
@@ -0,0 +1,12 @@
+# Accessing the list items 
+
+    # here accessing the list items using the index number.
+
+a = ["Prabhu", "Pritam", "Pradyumna","satabdi"]
+print("Accessing element from the list")
+print(a[0])
+print(a[2])
+
+print("Accessing element using negative indexing")
+print(a[-1])
+print(a[-3])
