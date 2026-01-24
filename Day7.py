6cf2b542a3a24cfd66ae1338023fd03c4cc63ce0
Prabhu <prabhuprasadkarmi@gmail.com>
2026-01-24 22:22:08 +0530

    learn the boolean datatypes

---
+++ Day7.py
@@ -0,0 +1,13 @@
+# Boolean Datatypes
+
+# there are two values(true,false).Where 1 is recognige as true and other hand 0 is recognize as the false
+
+print(type(True))
+print(type(False))
+
+
+if 1:
+    print("1 is truthy")
+
+if not 0:
+    print("0 is falsy")
