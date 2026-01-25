07fe7eb15af9d1f701f93db6b7f9e0b7e79a409d
Prabhu <prabhuprasadkarmi@gmail.com>
2026-01-25 19:29:45 +0530

    Learn Python Set Datatype

---
+++ Day8.py
@@ -0,0 +1,11 @@
+# Python Set Datatype
+    # set is unorder collection of data , where the data has not duplicate value.
+
+# initializing empty set
+s1 = set()
+
+s1 = set("Prabhuprasad Karmi")
+print("Set with the use of String: ", s1)
+
+s2 = set(["Saroj", "Prabhu", "Pritam"])
+print("Set with the use of List: ", s2)
