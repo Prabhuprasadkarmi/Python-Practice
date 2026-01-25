18369a30f9c2067771a2b1d66b8b5484b55c9e67
Prabhu <prabhuprasadkarmi@gmail.com>
2026-01-25 20:06:27 +0530

    Access the set datatypes

--- Day8.py
+++ Day8.py
@@ -4,8 +4,23 @@
 # initializing empty set
 s1 = set()
 
-s1 = set("Prabhuprasad Karmi")
-print("Set with the use of String: ", s1)
+s1 = set("Prabhuprasad Karmi")              # here space is count as a object
+print("Set with the use of String: ", s1) 
 
-s2 = set(["Saroj", "Prabhu", "Pritam"])
+s2 = set(["Saroj", "Prabhu", "Pradyumna"])
 print("Set with the use of List: ", s2)
+
+# Access the set datatypes
+""" Set items cannot be accessed by referring to an index, 
+     since sets are unordered the items have no index. But we can loop through the set items using a for loop, 
+     or ask if a specified value is present in a set, by using the keyword in."""
+
+set1 = set(["Saroj", "Prabhu", "Pradyumna","Prabhu"]) #Duplicates are removed automatically
+print(set1) 
+
+# loop through set
+for i in set1:
+   print(i, end=" ") #prints elements one by one
+  
+# check if item exist in set   
+print( "Prabhu" in set1)
