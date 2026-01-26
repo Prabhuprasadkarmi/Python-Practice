6ac8e49f6b98a260f7a789d23b9dced5e51f44fd
Prabhu <prabhuprasadkarmi@gmail.com>
2026-01-26 22:42:24 +0530

    Learn if else condition statement

--- Day9.py
+++ Day9.py
@@ -7,3 +7,17 @@
 age = 20
 if age >= 18:
     print("Eligible to vote.")
+
+# Short Hand if
+    # Short-hand if statement allows us to write a single-line if statement.
+
+age = 19
+if age > 18: print("Eligible to Vote.")
+
+# If else condition statement
+    # if the condition not satisfied then what block of code will be executed and what not.
+age = 10
+if age <= 12:
+    print("Travel for free.")
+else:
+    print("Pay for ticket.")
