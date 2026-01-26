d6ce88d6d1042d94ba460155f4b3c44f5672b34e
Prabhu <prabhuprasadkarmi@gmail.com>
2026-01-26 19:49:31 +0530

    Learn about the if conditional statement

---
+++ Day9.py
@@ -0,0 +1,9 @@
+# condiational statement in Python
+    # Used to execute certain of code on specific condition
+
+# If conditional statement
+    #If statement is the simplest form of a conditional statement. 
+    # It executes a block of code if the given condition is true.
+age = 20
+if age >= 18:
+    print("Eligible to vote.")
