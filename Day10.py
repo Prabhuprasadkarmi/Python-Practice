66a0b635e6aff499f815b913e5cbbd4b8f8ff0f8
Prabhu <prabhuprasadkarmi@gmail.com>
2026-01-27 20:22:04 +0530

    learn the shorthand if-else

---
+++ Day10.py
@@ -0,0 +1,5 @@
+# short hand if else
+    # It allow to write the code in single line 
+marks = 45
+res = "Pass" if marks >= 40 else "Fail"
+print(f"Result: {res}") #The "f" before the string enables f-string formatting.
