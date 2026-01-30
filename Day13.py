23db097b8d9ff2643c3d1b0035ed972000440d36
Prabhu <prabhuprasadkarmi@gmail.com>
2026-01-30 19:53:45 +0530

    Learn the for loop

--- Day13.py
+++ Day13.py
@@ -4,3 +4,28 @@
 n = 4
 for i in range(0, n):
     print(i)
+
+li = ["Saroj", "Pradyumna", "Prabhu"]
+print("This is the list Element")
+for x in li:
+    print(x)
+    
+tup = ("Satish", "Pratap", "Prahalad")
+print("This is the Tuple Element")
+for x in tup:
+    print(x)
+    
+s = "Prabhuprasad"
+print("This is the String Element")
+for x in s:
+    print(x)
+    
+d = dict({'x':123, 'y':354})
+print("This is the Dictonary Element")
+for x in d:
+    print("%s  %d" % (x, d[x]))
+    
+set1 = {10, 30, 20}
+print("This is the Set Element")
+for x in set1:
+    print(x),
