a10e7c5880efb9d48ae7bdb87d348a46d3c6e3d3
Prabhu <prabhuprasadkarmi@gmail.com>
2026-01-31 21:26:18 +0530

    Learn while loop

---
+++ Day14.py
@@ -0,0 +1,7 @@
+# While Loop
+    # In Python, a while loop is used to execute a block of statements repeatedly until a given condition is satisfied. 
+    # When the condition becomes false, the line immediately after the loop in the program is executed.
+cnt = 0
+while (cnt < 3):
+    cnt = cnt + 1
+    print("Hello Prabhuprasad")
