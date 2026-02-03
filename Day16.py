687ce6689f33a3fa7b5e5a8bcd219632a3bf24ac
Prabhu <prabhuprasadkarmi@gmail.com>
2026-02-03 22:31:37 +0530

    Learn the continue statement

---
+++ Day16.py
@@ -0,0 +1,8 @@
+# loop control statements
+    # Loop control statements in Python are used to alter the flow of execution of loops.
+# continue statement
+    # The continue statement is used to skip the current iteration of a loop and move to the next iteration.
+for letter in 'Prabhuprasad':
+    if letter == 'a' or letter == 's':
+        continue
+    print('Current Letter :', letter)
