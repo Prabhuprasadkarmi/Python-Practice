990ae5a2b4e5578af6e69bb049ce6bdda39825d9
Prabhu <prabhuprasadkarmi@gmail.com>
2026-02-03 22:39:17 +0530

    Learn the break statement

---
+++ Day17.py
@@ -0,0 +1,8 @@
+# break statement
+    # The break statement is used to exit a loop prematurely when a certain condition is met.
+for letter in 'Prabhuprasad Karmi':
+    if letter == 'a' or letter == 's':
+        break
+
+print('Current Letter :', letter)
+
