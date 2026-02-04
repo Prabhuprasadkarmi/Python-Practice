e69f20c08af405e897411423a1717f1e8991050e
Prabhu <prabhuprasadkarmi@gmail.com>
2026-02-04 22:06:25 +0530

    Learning the Python function

---
+++ Day18.py
@@ -0,0 +1,8 @@
+# Python Function 
+  # A function is a block of reusable code that performs a specific task.
+  # Functions help in breaking down complex problems into smaller, manageable parts.    
+# Defining a Function
+def greet(name):
+    print("Hello, " + name + "! Welcome to Python programming.") 
+# Calling a Function
+greet("Prabhuprasad Karmi")
