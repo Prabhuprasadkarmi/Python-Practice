e853b6d635cf07c21c933659f85457076d3765cb
Prabhu <prabhuprasadkarmi@gmail.com>
2026-02-10 21:59:16 +0530

    Learn the recursive function

--- Day19.py
+++ Day19.py
@@ -8,3 +8,13 @@ def outer_function():
     
     inner_function()  # Calling the inner function
 outer_function()  # Calling the outer function
+
+# recursive function
+    # A recursive function is a function that calls itself in order to solve a problem.
+    # It typically has a base case that stops the recursion and a recursive case that breaks the problem into smaller subproblems.   
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1) 
+print("The factorial of 5 is:", factorial(5))
